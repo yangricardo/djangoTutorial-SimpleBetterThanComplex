6b00c43c75fb9f74dc0d6cf97573e30c76a2a02f
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2017-10-30 00:40:12 -0200

    Parte 4
    - menu do usuário não funciona

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'widget_tweaks',
+    'accounts',
     'boards',
 ]
 
@@ -125,3 +126,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+
+LOGIN_URL = 'login'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
